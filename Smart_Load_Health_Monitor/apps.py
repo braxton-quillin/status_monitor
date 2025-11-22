34ac63a563a9c5b4b6b777ac9d4d608104d7033c
braxton-quillin <bwq04@hotmail.com>
2025-11-21 18:18:06 -0600

    make django app

---
+++ Smart_Load_Health_Monitor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartLoadHealthMonitorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Smart_Load_Health_Monitor'
