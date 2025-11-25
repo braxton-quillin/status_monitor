3a247acc97b20303a0027e68dc61b15530896cb7
braxton-quillin <bwq04@hotmail.com>
2025-11-24 19:02:18 -0600

    add backend stuff

--- Smart_Load_Health_Monitor/models.py
+++ Smart_Load_Health_Monitor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Appliance(models.Model):
+
+    appliance = models.CharField(max_length=255)
+    model = models.CharField(max_length=255, null=True, blank=True)
+    serial = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    kit = models.CharField(max_length=255, null=True, blank=True)
+    service_date = models.DateField(null=True, blank=True)
+    service_repeat = models.IntegerField(null=True, blank=True)
+
+    notified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.appliance
